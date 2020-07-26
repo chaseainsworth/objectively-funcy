a56c5963f73b9afe12c9a1cbb9750c3cb2b9b7fe
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-07-26 07:19:43 -0400

    done

--- main.js
+++ main.js
@@ -1,7 +1,54 @@
 // Your code here.
 
+const person = {
+  
+}
 
+const personTwo = {
 
+}
+
+const getFirstName = function(person) {
+  return person.firstName;
+}
+
+const getLastName = function(person) {
+  return person.lastName;
+}
+
+const getFullName = function(person) {
+  return person.firstName + " " + person.lastName;
+}
+
+const setFirstName = function(person, name) {
+  return person.firstName = name;
+}
+
+const setAge = function(person, newAge) {
+  return person.age = newAge;
+}
+
+const giveBirthday = function(person, plusOne) {
+  if (person.hasOwnProperty('age') === false) {
+    return person.age = 1;
+  } else {
+    return person.age = person.age + 1;
+  }
+}
+
+const marry = function(person, personTwo) {
+    person.married = true;
+    personTwo.married = true;
+    person.spouseName = getFullName(personTwo); //?
+    personTwo.spouseName = getFullName(person); //?
+}
+
+const divorce = function(person, personTwo) {
+  person.married = false;
+  personTwo.married = false;
+  delete person.spouseName;
+  delete personTwo.spouseName;
+}
 
 
 // Our code here. Don't touch!
